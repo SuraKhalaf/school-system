229a5350466a065497c5c739c84e35202a0f1303
Sura <s11819597@stu.najah.edu>
2023-03-15 11:13:42 +0200

    Add L5-swagger to user controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,7 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="School System ApplicationAPI",
+ *    version="1.0.0",
+ *    description="Demo School System API",
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,6 +13,42 @@ use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
+    /** 
+ * @OA\Post(
+ * path="api/register",
+ * summary="Sign up",
+ * description="sign up by email, password, phone number",
+ * operationId="authSignup",
+ * tags={"Users"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Pass user credentials",
+ *    @OA\JsonContent(
+ *       required={"email","password","phone"},
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+ *       @OA\Property(property="phone", type="string", example="+10 12345 678"),
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=422,
+ *    description="Wrong credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="The email has already been taken.")
+ *        )
+ *     ),
+ * @OA\Response(
+ *    response=201,
+ *    description="User created response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="uuid", type="string", example="98b13a9a-61f8-4ea4-bc0e-260ab8f6d4e1"),
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+ *       @OA\Property(property="phone", type="string", example="+10 12345 678"),
+ *        )
+ *     )
+ * )
+ */
     public function store(StoreRegistrationRequest $request)
     {
         $validated = $request->validated();
@@ -23,6 +59,26 @@ class UserController extends Controller
         return new UserDetailResource($user);
     }
 
+    /** Show user profile
+ * @OA\Get(
+ * path="api/user-profile",
+ * summary="Show user profile",
+ * description="show email, password, phone number",
+ * tags={"Users"},
+ * @OA\Response(
+ *    response=200,
+ *    description="OK",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="uuid", type="string", example="98b13a9a-61f8-4ea4-bc0e-260ab8f6d4e1"),
+ *       @OA\Property(property="name", type="string", example="Sura Khalaf"),
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="phone", type="string", example="+10 12345 678"),
+ *       @OA\Property(property="created_at", example="2020-01-01"),
+ *       @OA\Property(property="updated_at", example="2020-01-01"),
+ *        )
+ *     ),
+ * ),
+ */
     public function show(): UserDetailResource
     {
         $user = auth()->user();
@@ -30,6 +86,32 @@ class UserController extends Controller
         return new UserDetailResource($user);
     }
 
+    /**
+ * Login User
+ * @OA\Post(
+ * path="api/login",
+ * summary="Log in",
+ * description="Login by email, password",
+ * operationId="authLogin",
+ * tags={"Users"},
+ * @OA\RequestBody(
+ *    required=true,
+ *    description="Pass user credentials",
+ *    @OA\JsonContent(
+ *       required={"email","password"},
+ *       @OA\Property(property="email", type="string", format="email", example="user1@mail.com"),
+ *       @OA\Property(property="password", type="string", format="password", example="PassWord12345"),
+ *    ),
+ * ),
+ * @OA\Response(
+ *    response=422,
+ *    description="Wrong credentials response",
+ *    @OA\JsonContent(
+ *       @OA\Property(property="message", type="string", example="email/password incorrect")
+ *        )
+ *     )
+ * )
+ */
     public function loginUser(StoreUserRequest $request): string
     {
         $validated = $request->validated();
@@ -40,9 +122,9 @@ class UserController extends Controller
                 'token' => $token,
             ];
 
-            return 'Welcome' . ' ' . response(new RoleResource($user), 201) . ' ' . response($response, 201);
+            return 'Welcome' . ' ' . response(new RoleResource($user)) . ' ' . response($response);
         }
 
-        return 'email/password incorrect';
+        return ''.response("email/password incorrect",422);
     }
 }
